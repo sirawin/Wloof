91acefbac1f8515a3107f83740bfa41bdaa942cf
Nithiwat Sirirattanachaikul <nitsirs@gmail.com>
2024-10-20 22:59:00 +0700

    Update init.js

---
+++ wloof/src/app/init.js
@@ -0,0 +1,22 @@
+import { useEffect } from 'react';
+import '../styles/globals.css'
+
+const liffId = '2006477399-yokJlXXe'
+
+function MyApp({ Component, pageProps }) {
+  useEffect(async () => {
+    const liff = (await import('@line/liff')).default
+    try {
+      await liff.init({ liffId });
+    } catch (error) {
+      console.error('liff init error', error.message)
+    }
+    if (!liff.isLoggedIn()) {
+      liff.login();
+    }
+  })
+
+  return <Component {...pageProps} />
+}
+
+export default MyApp
