e5599c2604d3c3f92f5922997c3f7f6dcb57d0d9
Nithiwat Sirirattanachaikul <nitsirs@gmail.com>
2024-10-21 21:35:07 +0700

    Update _app.js

--- wloof/pages/_app.js
+++ wloof/pages/_app.js
@@ -1,16 +1,20 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import '../styles/globals.css'
 
 const liffId = process.env.NEXT_PUBLIC_LIFF_ID
 
 function MyApp({ Component, pageProps }) {
+  const [liffObject, setLiffObject] = useState(null);
+  const [liffError, setLiffError] = useState(null);
   useEffect(async () => {
     const initLiff = async () => {
       const liff = (await import('@line/liff')).default;
       try {
       await liff.init({ liffId });
+      setLiffObject(liff);
       } catch (error) {
       console.error('liff init error', error.message);
+      setLiffError(error.message)
       }
       if (!liff.isLoggedIn()) {
       liff.login();
@@ -19,7 +23,7 @@ function MyApp({ Component, pageProps }) {
       initLiff();
   }, [])
 
-  return <Component {...pageProps} />
+  return <Component {...pageProps} liff={liffObject} liffError={liffError} />
 }
 
 export default MyApp
