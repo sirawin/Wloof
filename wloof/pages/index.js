0f5924a21743345738f294a64e3038d6af7cd9fd
Nithiwat Sirirattanachaikul <nitsirs@gmail.com>
2024-10-21 17:05:48 +0700

    npx

--- wloof/pages/index.js
+++ wloof/pages/index.js
@@ -1,33 +1,34 @@
-import { useEffect, useState } from 'react'
-import Head from 'next/head'
-import Image from 'next/image'
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
 
-
-export default function Profile() {
-  const [profile, setProfile] = useState({})
-
-  useEffect(async () => {
-    const liff = (await import('@line/liff')).default
-    await liff.ready
-    const profile = await liff.getProfile()
-    setProfile(profile)
-  }, [profile.userId])
-
+export default function Home({ liff, liffError }) {
   return (
-    <section>
+    <div>
       <Head>
-        <title>My Profile</title>
+        <title>LIFF App</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1>Profile</h1>
-      <div>
-        {profile.pictureUrl && <Image
-          src={profile.pictureUrl}
-          alt={profile.displayName}
-          width={500}
-          height={500}
-        />}
-        <div>Name: {profile.displayName}</div>
-      </div>
-    </section>
-  )
+
+      <main className={styles.main}>
+        <h1>create-liff-app</h1>
+        {liff && <p>LIFF init succeeded.</p>}
+        {liffError && (
+          <>
+            <p>LIFF init failed.</p>
+            <p>
+              <code>{liffError}</code>
+            </p>
+          </>
+        )}
+        <a
+          href="https://developers.line.biz/ja/docs/liff/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          LIFF Documentation
+        </a>
+      </main>
+    </div>
+  );
 }
