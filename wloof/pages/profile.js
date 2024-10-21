32c08baba49ff99a2495d069352747d5dfd0d3d3
Nithiwat Sirirattanachaikul <nitsirs@gmail.com>
2024-10-21 19:57:36 +0700

    Update profile.js

--- wloof/pages/profile.js
+++ wloof/pages/profile.js
@@ -3,18 +3,14 @@ import Head from 'next/head'
 import Image from 'next/image'
 
 
-
 export default function Profile() {
   const [profile, setProfile] = useState({})
 
   useEffect(async () => {
-    const setProfileF = async () => {
     const liff = (await import('@line/liff')).default
     await liff.ready
     const profile = await liff.getProfile()
     setProfile(profile)
-    }
-    setProfileF();
   }, [profile.userId])
 
   return (
